1cedb2aca95b6cd4b6c8c2d2755f1a1dd7141495
Spurrya Jaggi <spurrya@Spurryas-MacBook-Pro.local>
2023-10-12 16:15:29 -0400

    Adding utility functions and afterInitialize hook:

--- src/TakeProfitsHook.sol
+++ src/TakeProfitsHook.sol
@@ -6,8 +6,16 @@ import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.so
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
 
 contract TakeProfitsHook is BaseHook, ERC1155 {
+
+    using PoolIdLibrary for IPoolManager.PoolKey;
+
+    mapping(PoolId poolId => int24 tickLower) public tickLowerLasts;
+
+    mapping(PoolId poolId => mapping(int24 tick => mapping(bool zeroForOne => int256 amount))) public takeProfitPositions;
 
     // Initialize BaseHook and ERC1155 parent contracts in the constructor
     constructor(IPoolManager _poolManager, string memory _uri) BaseHook(_poolManager) ERC1155(_uri) {}
@@ -24,5 +32,30 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
                 beforeDonate: false,
                 afterDonate: false
         });
+    }
+
+    // Hooks
+    function afterInitialize(
+        address,
+        PoolKey calldata key,
+        uint160,
+        int24 tick,
+        // Add bytes calldata after tick
+        bytes calldata
+
+    ) external override poolManagerOnly returns (bytes4) {
+        _setTickLowerLast(key.toId(), _getTickLower(tick, key.tickSpacing));
+        return TakeProfitsHook.afterInitialize.selector;
+    }
+
+    // Utility Helpers
+    function _setTickLowerLast(PoolId poolId, int24 tickLower) private {
+        tickLowerLasts[poolId] = tickLower;
+    }
+
+    function _getTickLower(int24 actualTick, int24 tickSpacing) private pure returns (int24) {
+        int24 intervals = actualTick / tickSpacing;
+        if (actualTick < 0 && actualTick % tickSpacing != 0) intervals--; // round towards negative infinity
+        return intervals * tickSpacing;
     }
 }
