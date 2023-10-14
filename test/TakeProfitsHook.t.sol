f753936342475c245265ad34e8f7cbee580c3fd1
Spurrya Jaggi <spurrya@Spurryas-MacBook-Pro.local>
2023-10-14 17:28:25 -0400

    Added tests for initializing pool, adding liquidity to pool and set up

--- test/TakeProfitsHook.t.sol
+++ test/TakeProfitsHook.t.sol
@@ -64,6 +64,7 @@ contract TakeProfitsHookTest is Test, GasSnapshot {
     // This is used as the initial price for the pool 
     // as we add equal amounts of token0 and token1 to the pool during setUp
     uint160 constant SQRT_RATIO_1_1 = 79228162514264337593543950336;
+    bytes constant ZERO_BYTES = abi.encodePacked("");
 
 
     function _deployERC20Tokens() private {
@@ -79,6 +80,102 @@ contract TakeProfitsHookTest is Test, GasSnapshot {
             token0 = tokenB;
             token1 = tokenA;
         }
+    }
+
+    function _stubValidateHookAddress() private {
+        // Deploy the stub contract
+        TakeProfitsStub stub = new TakeProfitsStub(poolManager, hook);
+        
+        // Fetch all the storage slot writes that have been done at the stub address
+        // during deployment
+        (, bytes32[] memory writes) = vm.accesses(address(stub));
+
+        // Etch the code of the stub at the hardcoded hook address
+        vm.etch(address(hook), address(stub).code);
+
+        // Replay the storage slot writes at the hook address
+        unchecked {
+            for (uint256 i = 0; i < writes.length; i++) {
+                bytes32 slot = writes[i];
+                vm.store(address(hook), slot, vm.load(address(stub), slot));
+            }
+        }
+    }
+
+    function _initializePool() private {
+        // Deploy the test-versions of modifyPositionRouter and swapRouter
+        modifyPositionRouter = new PoolModifyPositionTest(IPoolManager(address(poolManager)));
+        swapRouter = new PoolSwapTest(IPoolManager(address(poolManager)));
+
+        // Specify the pool key and pool id for the new pool
+
+        /// Remove IPoolManager
+        poolKey = PoolKey({
+            currency0: Currency.wrap(address(token0)),
+            currency1: Currency.wrap(address(token1)),
+            fee: 3000,
+            tickSpacing: 60,
+            hooks: IHooks(hook)
+        });
+        poolId = poolKey.toId();
+        
+        // Initialize the new pool with initial price ratio = 1
+        poolManager.initialize(poolKey, SQRT_RATIO_1_1, ZERO_BYTES);
+    }
+
+    function _addLiquidityToPool() private {
+        // Mint a lot of tokens to ourselves
+        token0.mint(address(this), 100 ether);
+        token1.mint(address(this), 100 ether);
+
+        // Approve the modifyPositionRouter to spend your tokens
+        token0.approve(address(modifyPositionRouter), 100 ether);
+        token1.approve(address(modifyPositionRouter), 100 ether);
+
+        // Add liquidity across different tick ranges
+        // First, from -60 to +60
+        // Then, from -120 to +120
+        // Then, from minimum possible tick to maximum possible tick
+
+        // Add liquidity from -60 to +60
+        // PoolKey memory key, IPoolManager.ModifyPositionParams memory params, bytes memory hookData
+
+        modifyPositionRouter.modifyPosition(
+            poolKey,
+            IPoolManager.ModifyPositionParams(-60, 60, 10 ether),
+            ZERO_BYTES
+        );
+
+        // Add liquidity from -120 to +120
+        modifyPositionRouter.modifyPosition(
+            poolKey,
+            IPoolManager.ModifyPositionParams(-120, 120, 10 ether),
+            ZERO_BYTES
+        );
+
+        // Add liquidity from minimum tick to maximum tick
+        modifyPositionRouter.modifyPosition(
+            poolKey,
+            IPoolManager.ModifyPositionParams(
+                TickMath.minUsableTick(60),
+                TickMath.maxUsableTick(60),
+                50 ether
+            ),
+            ZERO_BYTES
+        );
+
+        // Approve the tokens for swapping through the swapRouter
+        token0.approve(address(swapRouter), 100 ether);
+        token1.approve(address(swapRouter), 100 ether);
+    }
+
+
+    function setUp() public {
+        _deployERC20Tokens();
+        poolManager = new PoolManager(500_000);
+        _stubValidateHookAddress();
+        _initializePool();
+        _addLiquidityToPool();
     }
 
 }
